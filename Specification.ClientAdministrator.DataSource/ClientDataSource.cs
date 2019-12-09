3bdc4f6f8fcb92b3d0aac3d8db15f6ba4a278bcf
jordinola <jordinola@JORDINOLA>
2019-12-08 19:57:10 -0500

    First commit

---
+++ Specification.ClientAdministrator.DataSource/ClientDataSource.cs
@@ -0,0 +1,22 @@
+﻿using Specification.ClientAdministrator.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Specification.ClientAdministrator.DataSource
+{
+    public class ClientDataSource
+    {
+        public List<Client> GetClientsData()
+        {
+            return new List<Client>
+            {
+                new Client { ClientId = 1, Name = "Juan", LastName = "Tomas", Age = 20},
+                new Client { ClientId = 2, Name = "Jose", LastName = "Roverson", Age = 25},
+                new Client { ClientId = 3, Name = "Ann", LastName = "Neyra", Age = 22},
+                new Client { ClientId = 4, Name = "Julie", LastName = "De La Rosa", Age = 30},
+                new Client { ClientId = 5, Name = "Rodrigo", LastName = "Perez", Age = 40},
+                new Client { ClientId = 6, Name = "Pepe", LastName = "Del Carpio", Age = 41}
+            };
+        }
+    }
+}
---
+++ Specification.ClientAdministrator.Repository/ClientRepository.cs
@@ -0,0 +1,19 @@
+﻿using Specification.ClientAdministrator.DataSource;
+using Specification.ClientAdministrator.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Specification.ClientAdministrator.Repository
+{
+    public class ClientRepository
+    {
+        private readonly List<Client> _data;
+
+        public ClientRepository()
+        {
+            _data = new ClientDataSource().GetClientsData();
+        }
+
+
+    }
+}
